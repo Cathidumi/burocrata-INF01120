c18958926c6a815f668e8b583db460c7024cfbd1
Vagner Bloise <bloisevagner@gmail.com>
2026-09-05 19:42:40 -0300

    Update Ata.java

--- Ata.java
+++ Ata.java
@@ -16,3 +16,45 @@ public class Ata extends Documento{
 }
 
 //getters
+    public int getNumero(){
+        return this.numero;
+    }
+
+    public String getTexto(){
+        return this.texto;
+    }
+
+    public String[] getPresente(){
+        return this.presentes;
+    }
+
+    // equals
+    @Override
+    public boolean equals(Object o) {
+        // testar o objeto
+        if (this == o) {
+            return true;
+        }
+
+        // testa se é da mesma classe
+        if (o == null || this.getClass() != o.getClass()) {
+            return false;
+        }
+
+        /* começo de código gerado por IA */
+        // verifica se os atributos da classe mãe são iguais
+        if (!super.equals(o)) {
+            return false;
+        }
+        /* fim de código gerado por IA */
+        
+        // verifica se Ata é uma Ata valida
+        Ata outro = (Ata) o;
+
+        /* começo de código gerado por IA */
+        // compara os 3 atributos de Ata em um único retorno usando &&
+        return this.numero == outro.numero && 
+               this.texto.equals(outro.texto) && 
+               Arrays.equals(this.presentes, outro.presentes);
+        /* fim de código gerado por IA */
+    }
