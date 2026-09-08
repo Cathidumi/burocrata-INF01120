c9a7f139f7dc054814949a49b0c0906b4c795fde
Vagner Bloise <bloisevagner@gmail.com>
2026-09-08 20:55:52 -0300

    Create Oficio class extending Deliberacao

---
+++ src/estudantes/entidades/Oficio.java
@@ -0,0 +1,48 @@
+package estudantes.entidades;
+
+import professor.entidades.CodigoCurso;
+import java.util.Objects;
+
+public class Oficio extends Deliberacao{
+    private String destinatario;
+
+    //construtor
+    public Oficio(String criador, CodigoCurso codigoCurso, int paginas, String texto, String destinatario){
+        super(criador, codigoCurso, paginas, texto);   //herança de todas as suas mães
+        this.destinatario = destinatario;
+    }
+
+    //getter
+    public getDestinatario(){
+        return this.destinatario;
+    }
+
+    //equals
+    @Override
+    public boolean equals(Object o){
+        //verifica se é o mesmo objeto
+        if(this == o){
+            return true;
+        }
+
+        //verifica se é nulo ou da mesma classe
+        if (o == null || this.getClass() != o.getClass()) {
+            return false;
+        }
+
+        // verifica se os atributos da classe mãe são iguais
+        if (!super.equals(o)) {
+            return false;
+        }
+
+        //verifica se Oficio é valido
+        Oficio outro = (Oficio) o;
+        return this.destinatario.equals(outro.destinatario);  //retorna o texto comparando-os
+    }
+
+    //hashcode
+    @Override
+    public int hashCode(){
+        return Object.hash(super.hashCode(), this.destinatario);
+    }
+}
