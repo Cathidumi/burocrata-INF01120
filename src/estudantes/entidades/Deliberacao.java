72b77848b5b7790fac21090593fc3ef5352a27d8
Vagner Bloise <bloisevagner@gmail.com>
2026-09-08 20:24:22 -0300

    Add Deliberacao class extending DocumentoAdministrativo
    Implement Deliberacao class extending DocumentoAdministrativo with a constructor, getter, and equals method.

---
+++ src/estudantes/entidades/Deliberacao.java
@@ -0,0 +1,43 @@
+package estudantes.entidades;
+
+import professor.entidades.CodigoCurso;
+
+//extensão de DocumentoAdministrativo
+public abstract class Deliberacao extends DocumentoAdministrativo{
+    private String texto;
+
+    //construtor
+    public Deliberacao(String criador, CodigoCurso codigoCurso, int paginas, String texto){
+        super(criador, codigoCurso, paginas);   //herança de documento
+        this.texto = texto;
+    } 
+
+    //getter
+    public String getTexto(){
+        return this.texto;
+    }
+
+    //equals
+    @Override
+    public boolean equals(Object o){
+        //verifica se é o mesmo objeto
+        if(this == o){
+            return true;
+        }
+
+        //verifica se é nulo ou da mesma classe
+        if (o == null || this.getClass() != o.getClass()) {
+            return false;
+        }
+
+        // verifica se os atributos da classe mãe são iguais
+        if (!super.equals(o)) {
+            return false;
+        }
+
+        //verifica se Deliberacao é valido
+        Deliberacao outra = (Deliberacao) o;
+
+
+
+    }
